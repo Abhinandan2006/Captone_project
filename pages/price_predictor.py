a6bae3ed11ca1536e3ca23c5e9caed8b7361c0ec
Abhinandan <dwivediabhinandan2006@gmail.com>
2025-12-03 22:36:50 +0530

    predictive model

--- pages/price_predictor.py
+++ pages/price_predictor.py
@@ -1,6 +1,37 @@
 import pandas as pd
+import numpy as np
 import streamlit as st
-import pickle
+import joblib
 
 df = pd.read_csv('data/final_df.csv')
-st.dataframe(df)
+pipeline = joblib.load('model/final_model.pkl')
+st.header('Enter your input')
+
+property_type = st.selectbox('property Type', ['flat', 'house'])
+sector = st.selectbox('Sector', sorted(df['sector'].unique().tolist()))
+bedroom = float(st.selectbox('Number of bedrooms', sorted(df['bedRoom'].unique().tolist())))
+bathroom = float(st.selectbox('Number of bathrooms', sorted(df['bathroom'].unique().tolist())))
+balcony = st.selectbox('Balconies', sorted(df['balcony'].unique().tolist()))
+property_age = st.selectbox('Property Age', sorted(df['agePossession'].unique().tolist()))
+built_up_area = float(st.number_input(label='Built Up Area'))
+servant_room = float(st.selectbox('Servant Room', [0.0, 1.0]))
+store_room = float(st.selectbox('Store Room', [0.0, 1.0]))
+furnishing_type = st.selectbox('Furnishing Type', sorted(df['furnishing_type'].unique().tolist()))
+luxury_category = st.selectbox('Luxury Category', sorted(df['luxury_category'].unique().tolist()))
+floor_category = st.selectbox('Floor Category', sorted(df['floor_category'].unique().tolist()))
+
+if st.button('Predict'):
+    data = [[property_type, sector, bedroom, bathroom, balcony, property_age, built_up_area,
+             servant_room, store_room, furnishing_type, luxury_category, floor_category]]
+    columns = ['property_type', 'sector', 'bedRoom', 'bathroom', 'balcony',
+               'agePossession', 'built_up_area', 'servant room', 'store room',
+               'furnishing_type', 'luxury_category', 'floor_category']
+
+    # Convert to DataFrame
+    one_df = pd.DataFrame(data, columns=columns)
+
+    base_price = np.expm1(pipeline.predict(one_df))[0]
+    low = base_price - 0.22
+    high = base_price + 0.22
+
+    st.text(f"The price of flat is in between {low:.2f} cr and {high:.2f} cr")
